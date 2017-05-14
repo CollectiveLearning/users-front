668df89a8a680b2d3771419c1695390bfa32678f
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-14 02:56:37 -0500

    main.js config

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App'
+import router from './router'
+
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+Vue.http.options.root = 'http://chat-backend-sena.herokuapp.com'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
